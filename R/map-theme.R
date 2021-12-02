b4d7f856e752f045b6ff18fbac831cfb2a123388
dungates <duncan.gates123@gmail.com>
2021-12-02 00:27:17 -0800

    add theme again 2

--- R/map-theme.R
+++ R/map-theme.R
@@ -4,14 +4,14 @@
 #' @param base_family, the base family
 #' @param bg background color
 #'
-#' @examples
+#' @examples \dontrun{
 #' library(sf)
 #' library(ggplot2)
 #' nc <- sf::read_sf(system.file("shape/nc.shp", package="sf"))
 #' ggplot(nc) +
 #'    geom_sf() +
 #'    theme_map()
-#'
+#' }
 #' @export
 theme_map <- function(base_family = "Fira Sans", base_size = 15, bg = "gray15") {
   ret <- ggplot2::theme_void(base_family = base_family, base_size = base_size)
@@ -20,6 +20,8 @@ theme_map <- function(base_family = "Fira Sans", base_size = 15, bg = "gray15") 
 
   ret <- ret + ggplot2::theme(plot.background = element_rect(fill = bg, color = bg),
                               panel.background = element_rect(fill = bg, color = bg))
+
+  ret <- ret + ggplot2::theme(plot.title = element_text(hjust = 0.5))
 
   ret
 }
