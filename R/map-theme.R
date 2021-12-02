1b5a9ac4ce99b91ca9f8f8567e68fc34956610d2
dungates <duncan.gates123@gmail.com>
2021-12-01 19:01:44 -0800

    add map theme, fix theme_duncan

---
+++ R/map-theme.R
@@ -0,0 +1,23 @@
+#' @title Mapping theme
+#' @description theme for making maps
+#' @param base_size the base font sizer
+#' @param base_family, the base family
+#' @param bg background color
+#'
+#' @examples
+#' nc <- st_read(system.file("shape/nc.shp", package="sf"))
+#' ggplot(nc) +
+#'    geom_sf() +
+#'    theme_map()
+#'
+#' @export
+theme_map <- function(base_family = "Fira Sans", base_size = 15, bg = "gray15") {
+  ret <- ggplot2::theme_void(base_family = base_family, base_size = base_size)
+
+  ret <- ret + ggplot2::theme(legend.position = "bottom")
+
+  ret <- ret + ggplot2::theme(plot.background = element_rect(fill = bg, color = bg),
+                              panel.background = element_rect(fill = bg, color = bg))
+
+  ret
+}
